1306df533b77ea0a188260611d58890e8f7800af
Sam Ellenrieder <sam.ell@web.de>
2017-11-02 14:42:33 +0000

    Initial Commit

---
+++ Glowing Plankton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Glowing Plankton
+//
+//  Created by Sam Ellenrieder on 02/11/2017.
+//  Copyright © 2017 Sam Ellenrieder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Glowing PlanktonTests/Glowing_PlanktonTests.swift
@@ -0,0 +1,36 @@
+//
+//  Glowing_PlanktonTests.swift
+//  Glowing PlanktonTests
+//
+//  Created by Sam Ellenrieder on 02/11/2017.
+//  Copyright © 2017 Sam Ellenrieder. All rights reserved.
+//
+
+import XCTest
+@testable import Glowing_Plankton
+
+class Glowing_PlanktonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
